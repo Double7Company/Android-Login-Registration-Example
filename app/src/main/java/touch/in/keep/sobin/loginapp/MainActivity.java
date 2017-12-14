92fda9bc323e18828e040d17b5135c7dd55e0788
Sobin <sobinmathew988@gmail.com>
2017-12-14 17:06:40 +0530

    Toolbar added

--- app/src/main/java/touch/in/keep/sobin/loginapp/MainActivity.java
+++ app/src/main/java/touch/in/keep/sobin/loginapp/MainActivity.java
@@ -2,6 +2,8 @@ package touch.in.keep.sobin.loginapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
